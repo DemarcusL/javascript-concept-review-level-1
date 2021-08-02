be92d2414524c4a4f610619505d48af688710445
Demarcus Lester <demarcuslester12@gmail.com>
2021-08-02 14:49:54 -0500

    Java Attempt

---
+++ main.js
@@ -0,0 +1,55 @@
+// function main() {
+// // this will main function will hold my solutions!
+// ex1();
+// ex2();
+// }
+
+// //Do what that test is asking!
+// function ex1() {
+
+// }
+
+// //keep names related to exercise!
+// function ex2() {
+
+// }
+
+// //POE!
+// main();
+
+
+function main() {
+ex1();
+
+
+}
+
+
+//in example 1, when need to have a user input wo number. then it will interate and give the intergers between the two back to the user
+//I need to set numbers in an array for the iteration
+const lowNum = ["1","2","3","4","5"]
+const highNum = ["6","7","8","9","10"]
+// console.log(lowNum);
+// console.log(highNum);
+
+function ex1() {
+let lowNumInput = prompt('Enter a number between 1 - 5');    
+// let highNumInput = prompt('Enter a number between 5 - 10');
+for (let i = 0; i < lowNum.length; index++) {
+
+    
+}
+// for (let r = 0; r > highNum.length; index++) {
+//     let highNumInput = array[r];
+//     return [r];
+//     console.log[r];
+        
+// }
+
+console.log[i + r];
+alert(`You have entered ${lowNumInput} for your lowest value, and you entered ${highNumInput} for your highest value !`);
+
+}
+
+//POE
+ex1();
